7a1d04f54ad51dbfdac466688ef527628231ecd5
clheard54 <clarion.heard@gmail.com>
2020-02-24 15:19:33 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def list_view
+    if self.artist.name
+      listing = self.artist.name + ' - ' + self.title
+    else
+      listing = 'Unknown artist - ' + self.title
+    end
+    listing
+  end
 end
